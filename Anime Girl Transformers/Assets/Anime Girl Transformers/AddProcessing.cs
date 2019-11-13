e21d57d608038b63817f69c1c2bfaed9ffddc9c7
Xander Kakris <xand3rk6i6@gmail.com>
2019-11-13 16:27:33 +1100

    environment update

---
+++ Anime Girl Transformers/Assets/Anime Girl Transformers/AddProcessing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+public class AddProcessing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    Camera[] cameras;
+    void Update()
+    {
+        cameras = GameObject.FindObjectsOfType<Camera>();
+        foreach(Camera cam in cameras)
+        {
+            if (cam.isActiveAndEnabled)
+            {
+                cam.gameObject.AddComponent<PostProcessingBehaviour>();
+            }
+        }
+    }
+}
